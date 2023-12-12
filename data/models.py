73c306a507f93b574fed8f0a001a9c78e2afea5e
Ebrahim Fadly <96057410+MerSake@users.noreply.github.com>
2023-12-12 20:37:13 +0200

    N/A

--- data/models.py
+++ data/models.py
@@ -3,8 +3,6 @@ from . import db
 
 
 class User(db.Model, UserMixin):
-    __tablename__ = "user"
-
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     email = db.Column(db.String(45), unique=True, nullable=False)
     firstname = db.Column(db.String(20), nullable=False)
